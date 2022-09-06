a94971694659adfab3549394749933f6140c9ad1
fabianairc <fabianaramirezzz@gmail.com>
2022-09-05 22:50:35 -0300

    inuseEffect

--- dojo/src/Home.js
+++ dojo/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 
 const Home = () => {
@@ -7,10 +7,14 @@ const Home = () => {
     { title: "A blog", body: "Lorem...", author: "ra", id: "2" },
     { title: "the other blog", body: "Lorem...", author: "ta", id: "3" },
   ]);
-
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blogs.id !== id);
+    setBlogs(newBlogs);
+  };
+  useEffect(() => {});
   return (
     <div className="Home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handle delete={handleDelete} />
     </div>
   );
 };
--- dojo/src/blogList.js
+++ dojo/src/blogList.js
@@ -4,15 +4,16 @@ const BlogList = (props) => {
     const blogs=  props.blogs
     const title =props.title
 
-    <div. className ="blog-list">
+    <div. className= "blog-list">
 
-      { blogs.map((blog) => {
+      {blogs.map((blog) => {
         return (
             
           <div className="blog-preview" key={blog.id}>
             <h1>{title}</h1>
             <h1> {blog.title}</h1>
             <h2> Written by {blog.author}</h2>
+            <button onClick={()=> handleDelete (blog.id)}></button>
           </div>
         
         )
